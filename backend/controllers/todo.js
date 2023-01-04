634981dfe913bc4eb933695cf26f5bc5a913491f
Teheskhiel Fritz <teheskhielfritz@gmail.com>
2023-01-04 09:35:05 +0700

    start the frontend

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -2,7 +2,7 @@ import { Todo } from '../models/todo.js'
 
 export const getTodos = async(req, res) => {
     try {
-        const todos = Todo.find({})
+        const todos = await Todo.find({})
         res.status(200).json({ data: todos })
     } catch (err) {
         res.status(400).json({ data: err.message })
