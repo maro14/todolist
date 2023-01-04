35cb67726436616a3c09f22553f69895a5c92740
Teheskhiel Fritz <teheskhielfritz@gmail.com>
2023-01-04 18:33:33 +0700

    add css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,41 @@
+import{ useEffect, useState} from 'react'
 
+const API = "http://localhost:3001"
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState("")
+
+  useEffect(() => {
+    GetTodos()
+  })
+
+  const GetTodos = () => {
+    fetch(API + '/todos')
+    .then(res => res.json())
+    .then(data => setTodos(data))
+    .catch(err => console.error("Error", err))
+  }
+
+  const addTodo = async() => {
+    const data = await fetch(API + "/todo/add", )
+  }
+
   return (
     <div className="App">
+      <h1>Welcome, Friends</h1>
+      <h4>Your Task </h4>
+
+      <div className='todos'>
+
+        <div className='todo'>
+          <div className='text'>Get todo on list</div>
+
+          <div className='delete-todo'></div>
+        </div>
+        
+      </div>
       
     </div>
   );
